a60bc533bee66ac25d3bd179acd499614a4986f3
javiiiee <javier.bonilla98@gmail.com>
2017-09-05 19:21:00 -0400

    Initial commit

---
+++ src/Deux.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Deux {
+    public static void main(String [] args){
+        System.out.println("Enter your username?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Enter Password?");
+        Scanner two = new Scanner(System.in);
+        String password = two.next();
+        System.out.println("Hello javi, welcome to CSC200! and your password is Saints98");
+
+    }
+}
